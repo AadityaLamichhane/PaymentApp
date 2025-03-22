e2af2fe75285d62945bdad736ea452c447d8f733
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2025-03-23 00:02:34 +0545

    Fixing build Command and dependencies mess

--- apps/merchant-app/provider.tsx
+++ apps/merchant-app/provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
-console.log("This application is working properly ")
 export const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <RecoilRoot>

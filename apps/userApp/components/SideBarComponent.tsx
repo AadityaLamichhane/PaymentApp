b0d966cd0e01de815965d656020d35d577755537
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2025-03-23 00:22:58 +0545

    ch

--- apps/userApp/components/SideBarComponent.tsx
+++ apps/userApp/components/SideBarComponent.tsx
@@ -8,13 +8,13 @@ export const SideBarComponent = ({ href, title, icon }: { href: string; title: s
     const selected = pathname === href;
 
     return (
-        <div className={` flex items-center min-w-100% max-w-[250] cursor-pointer p-2 pl-6 transition-all duration-300 ${selected ? "border-l-4 border-blue-500 bg-blue-100 text-blue-500" : "text-black"}`} 
+        <div className={`flex items-center min-w-100% max-w-[250] cursor-pointer p-2 pl-6 transition-all duration-300 ${selected ? "border-l-4 bg-blue-500 bg-blue-100 text-white" : "text-black"}`} 
             onClick={() => router.push(href)}
         >
             <div className="mr-3 md:justify-center items-center">
-                <div>
-                    {icon}
-                </div>
+            <div>
+                {icon}
+            </div>
             </div>
             <div className="font-bold text-sm hidden sm:block">{title}</div>
         </div>
